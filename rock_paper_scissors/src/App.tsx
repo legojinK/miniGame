40cb077ef4bfd0ef1f9eb83d3cc032810770f246
heejinkim <hjkim2@okestro.com>
2024-08-13 18:17:19 +0900

    [feat] 가위바위보 게임 -2

--- rock_paper_scissors/src/App.tsx
+++ rock_paper_scissors/src/App.tsx
@@ -11,7 +11,7 @@ export type Choice = {
     name: string;
     img: string;
 };
-
+export type resultType = "tie" | "winner" | "loser" |null;
 interface ChoiceMap {
     rock: Choice;
     scissors: Choice;
@@ -34,15 +34,60 @@ const choice:ChoiceMap={
     }
 }
 function App() {
-    const [userSelect,setUserSelect] = useState<Choice | null>(null)
+    const [userSelect,setUserSelect] = useState<Choice | null>(choice.paper)
+    const [comSelect,setComSelect] = useState<Choice | null>(choice.paper)
+    const [result,setResult] = useState<resultType|null>(null)
+    const [comeResult,setComResult] = useState<resultType|null>(null)
     const play=(userChoice: keyof ChoiceMap)=>{
-        setUserSelect(choice[userChoice])
+
+            const userChoiceResult = choice[userChoice];
+            setUserSelect(userChoiceResult);
+
+            const comChoiceResult = randomChoice();
+            setComSelect(comChoiceResult);
+
+            const userGameResult = judgement(userChoiceResult, comChoiceResult);
+            setResult(userGameResult);
+
+            setComResult(comJudgement(userGameResult));
+
     }
-  return (
+    const randomChoice=()=>{
+            let itemArray = Object.keys(choice)as Array<keyof ChoiceMap>
+            let randomNum =Math.floor(Math.random()*(itemArray.length))
+            let finalChoice = itemArray[randomNum]
+        return (choice[finalChoice])
+    }
+
+    const judgement=(user: { name: any; img?: string; }, computer: { name: any; img?: string; })=>{
+        if (user.name===computer.name){
+            return "tie";
+        }else if (user.name==="rock"){
+            return computer.name ==='scissors'?"winner" :"loser"
+        }else if (user.name==="scissors"){
+            return computer.name ==='paper'?"winner" :"loser"
+        }else if (user.name==="paper"){
+            return computer.name ==='rock'?"winner" :"loser"
+        }
+        return "tie";
+    }
+    const comJudgement = (result: resultType): resultType => {
+        if (result === "tie") {
+            return "tie";
+        } else if (result === "winner") {
+            return "loser";
+        } else if (result === "loser") {
+            return "winner";
+        }
+        return "tie";
+    };
+
+
+    return (
       <div>
         <div className="main">
-            <Box title="you" item={userSelect}/>
-            {/*<Box title="computer" item={userSelect}/>*/}
+            <Box title="you" item={userSelect} result={result}/>
+            <Box title="computer" item={comSelect} result={comeResult}/>
         </div>
         <div>
             <button onClick={()=>play("scissors")} >가위</button>
--- rock_paper_scissors/src/component/Box.tsx
+++ rock_paper_scissors/src/component/Box.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import  {Choice}  from '@/App';
+import {Choice} from '@/App';
 
 type BoxProps = {
     title?: string
     item?: Choice | null
+    result?: string| null
+    comeResult?: string| null
 };
-const Box = ({ title,item }: BoxProps) => {
-    console.log("props", { title,item });
+const Box = ({ title,item,result,comeResult }: BoxProps) => {
+    const boxClassName =
+        result === "winner" || comeResult === "winner"
+            ? "box winner"
+            : result === "loser" || comeResult === "loser"
+                ? "box loser"
+                : "box";
     return (
-        <div className="box" >
+        <div className={boxClassName} >
             <h1>{title}</h1>
-            <img className="img-container" alt="가위" src={item?.img} />
-            <h2>win</h2>
+            <img className="img-container" alt="이미지" src={item?.img} />
+            <h2>{result || comeResult}</h2>
         </div>
     );
 };
